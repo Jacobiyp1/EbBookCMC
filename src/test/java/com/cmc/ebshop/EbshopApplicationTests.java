f0ec516accd9608d72017433ead56d2263bb2946
tuanframgia@gmail.com <Jacobiyp1>
2022-04-15 11:03:58 +0700

    init project

---
+++ src/test/java/com/cmc/ebshop/EbshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmc.ebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
