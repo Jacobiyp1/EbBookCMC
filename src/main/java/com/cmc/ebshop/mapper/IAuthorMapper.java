5081d67ed5c288776f0c0df0b87263e57600880c
Jacobiyp1 <103098727+Jacobiyp1@users.noreply.github.com>
2022-04-19 15:17:12 +0700

    feat/getAuthorApi (#6)
    Co-authored-by: Long. Nguyen Vu - CMC Global DU1.3 <nvlong3@cmcglobal.vn>

--- src/main/java/com/cmc/ebshop/mapper/IAuthorMapper.java
+++ src/main/java/com/cmc/ebshop/mapper/IAuthorMapper.java
@@ -1,7 +1,8 @@
 package com.cmc.ebshop.mapper;
 
 import com.cmc.ebshop.dto.Author.AuthorDto;
-import com.cmc.ebshop.entity.Book;
+import com.cmc.ebshop.dto.BookDto;
+import com.cmc.ebshop.entity.Author;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -12,9 +13,9 @@ public interface IAuthorMapper {
     void insertAuthor(@Param("author") AuthorDto authorDto);
     void updateAuthor(@Param("author") AuthorDto authorDto);
     void deleteAuthor(@Param("author") AuthorDto authorDto);
-    AuthorDto getAuthor(@Param("id") String id);
+    Author getAuthor(@Param("id") String id);
     int getNumberOfBookBelongToAuthor(@Param("id") String id);
-    List<Book> top3BestSale(@Param("id") String id);
+    List<BookDto> top3BestSale(@Param("id") String id);
     boolean isExistsAuthor(@Param("id") String id);
     boolean isExistsBooksOfAuthor(@Param("id") String id);
 }
