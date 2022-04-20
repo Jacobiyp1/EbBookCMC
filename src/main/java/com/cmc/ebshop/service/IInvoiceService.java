78266b2c5078bb93020ee4eb0ffcdb0ab6facfa5
tuanframgia@gmail.com <Jacobiyp1>
2022-04-20 08:44:33 +0700

    statistics book

--- src/main/java/com/cmc/ebshop/service/IInvoiceService.java
+++ src/main/java/com/cmc/ebshop/service/IInvoiceService.java
@@ -2,8 +2,6 @@ package com.cmc.ebshop.service;
 
 import com.cmc.ebshop.dto.response.invoice.InvoiceResponse;
 
-import java.util.List;
-
 public interface IInvoiceService {
     InvoiceResponse getInvoice(String orderId);
 }
