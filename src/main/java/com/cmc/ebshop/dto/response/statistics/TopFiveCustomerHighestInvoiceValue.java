5365e5e31f82bca8d37a844fa8f8209372047cfb
tuanframgia@gmail.com <Jacobiyp1>
2022-04-21 13:47:56 +0700

    statistics

--- src/main/java/com/cmc/ebshop/dto/response/statistics/TopFiveCustomerHighestInvoiceValue.java
+++ src/main/java/com/cmc/ebshop/dto/response/statistics/TopFiveCustomerHighestInvoiceValue.java
@@ -2,8 +2,14 @@ package com.cmc.ebshop.dto.response.statistics;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class TopFiveCustomerHighestInvoiceValue {
-    private String namePublisher;
-    private Integer quantitySold;
+    private String nameCustomer;
+    private String email;
+    private String phone;
+    private String address;
+    private Integer totalQuantity;
+    private BigDecimal totalPrice;
 }
