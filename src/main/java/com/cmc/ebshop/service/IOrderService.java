a18bdac16e402463129adf632694762436122990
tuanframgia@gmail.com <Jacobiyp1>
2022-04-21 14:08:46 +0700

    fix order

--- src/main/java/com/cmc/ebshop/service/IOrderService.java
+++ src/main/java/com/cmc/ebshop/service/IOrderService.java
@@ -1,7 +1,8 @@
 package com.cmc.ebshop.service;
 
+import com.cmc.ebshop.common.exception.NotFoundException;
 import com.cmc.ebshop.dto.request.order.OrderRequest;
 
 public interface IOrderService {
-    OrderRequest createOrder(OrderRequest orderRequest);
+    OrderRequest createOrder(OrderRequest orderRequest) throws NotFoundException;
 }
