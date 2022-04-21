378ed95aa4b538d90c5e6d5a716a8b42003141da
Jacobiyp1 <103098727+Jacobiyp1@users.noreply.github.com>
2022-04-21 13:48:37 +0700

    Feature/statistics (#10)
    * get invoice book
    
    * statistics book
    
    * statistics book
    
    * delete file java
    
    * api statistics
    
    * statistics
    
    Co-authored-by: tuanframgia@gmail.com <Jacobiyp1>

--- src/main/java/com/cmc/ebshop/controller/StatisticsController.java
+++ src/main/java/com/cmc/ebshop/controller/StatisticsController.java
@@ -1,8 +1,7 @@
 package com.cmc.ebshop.controller;
 
 import com.cmc.ebshop.dto.ResponseBodyDto;
-import com.cmc.ebshop.dto.response.invoice.InvoiceResponse;
-import com.cmc.ebshop.dto.response.statistics.TopTenSellingBooks;
+import com.cmc.ebshop.dto.response.statistics.*;
 import com.cmc.ebshop.service.IStatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,9 +17,33 @@ public class StatisticsController {
     @Autowired
     private IStatisticsService statisticsService;
 
-    @GetMapping
+    @GetMapping("/top-ten-selling-books")
     public ResponseEntity<ResponseBodyDto<TopTenSellingBooks>> getTopTenSellingBooks() {
         ResponseBodyDto<TopTenSellingBooks> responseBodyDto = new ResponseBodyDto("Success", HttpStatus.OK.value(), statisticsService.getTopTenSellingBooks());
+        return new ResponseEntity<>(responseBodyDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/top-five-author-selling-books")
+    public ResponseEntity<ResponseBodyDto<TopFiveAuthorSellingBooks>> getTopFiveAuthorSellingBooks() {
+        ResponseBodyDto<TopFiveAuthorSellingBooks> responseBodyDto = new ResponseBodyDto("Success", HttpStatus.OK.value(), statisticsService.getTopFiveAuthorSellingBooks());
+        return new ResponseEntity<>(responseBodyDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/top-five-publisher-selling-books")
+    public ResponseEntity<ResponseBodyDto<TopFivePublisherSellingBooks>> getTopFivePublisherSellingBooks() {
+        ResponseBodyDto<TopFivePublisherSellingBooks> responseBodyDto = new ResponseBodyDto("Success", HttpStatus.OK.value(), statisticsService.getTopFivePublisherSellingBooks());
+        return new ResponseEntity<>(responseBodyDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/top-five-customer-buy-books")
+    public ResponseEntity<ResponseBodyDto<TopFiveCustomerBuyBooks>> getTopFiveCustomerBuyBooks() {
+        ResponseBodyDto<TopFiveCustomerBuyBooks> responseBodyDto = new ResponseBodyDto("Success", HttpStatus.OK.value(), statisticsService.getTopFiveCustomerBuyBooks());
+        return new ResponseEntity<>(responseBodyDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/top-five-customer-highest-invoice-value")
+    public ResponseEntity<ResponseBodyDto<TopFiveCustomerHighestInvoiceValue>> getTopFiveCustomerHighestInvoiceValue() {
+        ResponseBodyDto<TopFiveCustomerHighestInvoiceValue> responseBodyDto = new ResponseBodyDto("Success", HttpStatus.OK.value(), statisticsService.getTopFiveCustomerHighestInvoiceValue());
         return new ResponseEntity<>(responseBodyDto, HttpStatus.OK);
     }
 }
