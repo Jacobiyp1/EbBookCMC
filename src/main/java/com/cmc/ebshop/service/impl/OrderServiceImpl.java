b391196ee4bdb9e7bf61553f5c4060df218e628e
Jacobiyp1 <103098727+Jacobiyp1@users.noreply.github.com>
2022-04-21 14:09:55 +0700

    fix order (#11)
    Co-authored-by: tuanframgia@gmail.com <Jacobiyp1>

--- src/main/java/com/cmc/ebshop/service/impl/OrderServiceImpl.java
+++ src/main/java/com/cmc/ebshop/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cmc.ebshop.service.impl;
 
+import com.cmc.ebshop.common.exception.NotFoundException;
 import com.cmc.ebshop.dto.request.book.BookRequest;
 import com.cmc.ebshop.dto.request.order.OrderRequest;
 import com.cmc.ebshop.dto.response.book.BookDto;
@@ -14,6 +15,7 @@ import com.cmc.ebshop.service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -34,21 +36,30 @@ public class OrderServiceImpl implements IOrderService {
     IBookMapper bookMapper;
 
     @Override
-    public OrderRequest createOrder(OrderRequest orderRequest) {
+    public OrderRequest createOrder(OrderRequest orderRequest) throws NotFoundException {
+        CustomerDto customerDto = customerMapper.findByEmail(orderRequest.getCustomerRequest().getEmail());
+        Orders order = orderRequest.toOrder(customerDto.getId());
+
+        List<OrderDetail> orderDetails = new ArrayList<>();
+        List<BookDto> bookDtos = new ArrayList<>();
+        for (BookRequest bookRequest : orderRequest.getBookRequests()) {
+            BookDto bookDto = bookMapper.findById(bookRequest.getId());
+            if (bookRequest.getBuyQuantity() > bookDto.getQuantityCurrent()) {
+                throw new NotFoundException("The book is out of stock");
+            }
+            orderDetails.add(new OrderDetail(UUID.randomUUID().toString(), order.getId(), bookRequest.getId(), bookRequest.getBuyQuantity(), bookDto.getPrice().multiply(BigDecimal.valueOf(bookRequest.getBuyQuantity()))));
+            bookDto.setQuantitySold(bookDto.getQuantitySold() + bookRequest.getBuyQuantity());
+            bookDtos.add(bookDto);
+        }
+        //update book
+        for (BookDto bookDto : bookDtos) {
+            bookMapper.updateBook(bookDto);
+        }
         // create customer if they don't exist
         if (!customerMapper.isExistsCustomer(orderRequest.getCustomerRequest().getEmail())) {
             customerMapper.insertCustomer(orderRequest.getCustomerRequest().toCustomer());
         }
-        CustomerDto customerDto = customerMapper.findByEmail(orderRequest.getCustomerRequest().getEmail());
-        Orders order = orderRequest.toOrder(customerDto.getId());
         orderMapper.insertOrder(order);
-        List<OrderDetail> orderDetails = new ArrayList<>();
-        for (BookRequest bookRequest : orderRequest.getBookRequests()) {
-            BookDto bookDto = bookMapper.findById(bookRequest.getId());
-            orderDetails.add(new OrderDetail(UUID.randomUUID().toString(), order.getId(), bookRequest.getId(), bookRequest.getBuyQuantity(), bookDto.getPrice()));
-            bookDto.setQuantitySold(bookDto.getQuantitySold() + bookRequest.getBuyQuantity());
-            bookMapper.updateBook(bookDto);
-        }
         orderDetailMapper.createOrderDetail(orderDetails);
         return orderRequest;
     }
